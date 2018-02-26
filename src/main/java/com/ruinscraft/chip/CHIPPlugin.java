1c2fa9c059ba0b2df5af3371009104048135bf2e
Dexton Anderson <contact@dextonanderson.com>
2018-02-26 08:39:58 -0600

    chunk inspection now optional

--- src/main/java/com/ruinscraft/chip/CHIPPlugin.java
+++ src/main/java/com/ruinscraft/chip/CHIPPlugin.java
@@ -12,6 +12,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.ruinscraft.chip.listeners.PlayerListener;
+import com.ruinscraft.chip.packetadapters.ChunkDataPacketAdapter;
 import com.ruinscraft.chip.packetadapters.HeldItemChangePacketAdapter;
 import com.ruinscraft.chip.packetadapters.SetCreativeSlotPacketAdapter;
 import com.ruinscraft.chip.packetadapters.SpawnEntityPacketAdapter;
@@ -23,11 +24,14 @@ public class CHIPPlugin extends JavaPlugin implements CommandExecutor {
 	public static final ChatColor COLOR_ERROR = ChatColor.RED;
 	public static final ChatColor COLOR_BASE = ChatColor.YELLOW;
 	
+	// non-command permissions
 	public static final String PERMISSION_BYPASS = "chip.bypass";
 	public static final String PERMISSION_NOTIFY = "chip.notify";
 
 	@Override
 	public void onEnable() {
+		saveDefaultConfig();
+		
 		PluginManager pluginManager = getServer().getPluginManager();
 
 		if (pluginManager.getPlugin("ProtocolLib") == null) {
@@ -42,6 +46,13 @@ public class CHIPPlugin extends JavaPlugin implements CommandExecutor {
 		protocolManager.addPacketListener(new SpawnEntityPacketAdapter(this));
 		protocolManager.addPacketListener(new HeldItemChangePacketAdapter(this));
 		protocolManager.addPacketListener(new UseItemPacketAdapter(this));
+		
+		boolean useChunkData = getConfig().getBoolean("chunk_load_inspection");
+		
+		if (useChunkData) {
+			protocolManager.addPacketListener(new ChunkDataPacketAdapter(this));
+			getLogger().log(Level.INFO, "Using chunk load inspection (experimental). This checks inventory holding blocks in loading chunks. Expect lag from this feature.");
+		}
 
 		pluginManager.registerEvents(new PlayerListener(), this);
 		
