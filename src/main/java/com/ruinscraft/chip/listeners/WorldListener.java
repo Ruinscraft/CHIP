a9a754fe385bb6da9876417402ae1d554f971712
Dexton Anderson <contact@dextonanderson.com>
2018-04-02 19:38:43 -0500

    cancel block physics

--- src/main/java/com/ruinscraft/chip/listeners/WorldListener.java
+++ src/main/java/com/ruinscraft/chip/listeners/WorldListener.java
@@ -1,8 +1,6 @@
 package com.ruinscraft.chip.listeners;
 
-import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPhysicsEvent;
@@ -11,43 +9,27 @@ public class WorldListener implements Listener {
 
 	@EventHandler
 	public void onBlockPhysics(BlockPhysicsEvent event) {
-		
 		Block block = event.getBlock();
 		
-		if (block.getType() == Material.SPONGE) {
+		if (block.getType().name().toLowerCase().contains("water")) {
 			event.setCancelled(true);
 		}
 		
-		if (block.getType() == Material.TNT) {
+		if (block.getType().name().toLowerCase().contains("lava")) {
 			event.setCancelled(true);
 		}
 		
-		if (block.getType() == Material.WATER) {
-			if (block.getRelative(BlockFace.DOWN).getType() == Material.SPONGE) {
-				event.setCancelled(true);
-			}
-			
-			if (block.getRelative(BlockFace.UP).getType() == Material.SPONGE) {
-				event.setCancelled(true);
-			}
-			
-			if (block.getRelative(BlockFace.EAST).getType() == Material.SPONGE) {
-				event.setCancelled(true);
-			}
-			
-			if (block.getRelative(BlockFace.WEST).getType() == Material.SPONGE) {
-				event.setCancelled(true);
-			}
-			
-			if (block.getRelative(BlockFace.NORTH).getType() == Material.SPONGE) {
-				event.setCancelled(true);
-			}
-			
-			if (block.getRelative(BlockFace.SOUTH).getType() == Material.SPONGE) {
-				event.setCancelled(true);
-			}
+		if (block.getType().name().toLowerCase().contains("tnt")) {
+			event.setCancelled(true);
 		}
 		
+		if (block.getType().name().toLowerCase().contains("sponge")) {
+			event.setCancelled(true);
+		}
+
+		if (event.isCancelled()) {
+			System.out.println("canceled blockphysics: " + block.getType().name() + " -> "+ block.getX() + "," + block.getY() + "," + block.getZ());
+		}
 	}
-	
+
 }
