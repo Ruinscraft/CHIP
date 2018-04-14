165b27c53cd90d4330430c7515647930384f5b63
Dexton Anderson <contact@dextonanderson.com>
2018-04-14 10:55:48 -0500

    check items when dispensed

--- src/main/java/com/ruinscraft/chip/listeners/WorldListener.java
+++ src/main/java/com/ruinscraft/chip/listeners/WorldListener.java
@@ -1,11 +1,15 @@
 package com.ruinscraft.chip.listeners;
 
+import java.util.Optional;
 import java.util.logging.Level;
 
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.block.BlockPhysicsEvent;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
 
 import com.ruinscraft.chip.ChipPlugin;
 
@@ -41,6 +45,31 @@ public class WorldListener implements Listener {
 		
 		if (event.isCancelled() && ChipPlugin.getInstance().notifyConsoleWhenCancelled) {
 			ChipPlugin.getInstance().getLogger().log(Level.INFO, "Canceled blockphysics: " + block.getType().name() + " -> "+ block.getX() + "," + block.getY() + "," + block.getZ());
+		}
+	}
+	
+	@EventHandler
+	public void onBlockDispense(BlockDispenseEvent event) {
+		final ItemStack itemStack = event.getItem();
+		
+		if (itemStack == null) {
+			return;
+		}
+		
+		if (ChipPlugin.hasModifications(itemStack)) {
+			event.setCancelled(true);
+			
+			final Block block = event.getBlock();
+			
+			if (block == null) {
+				return;
+			}
+			
+			if (block.getState() instanceof InventoryHolder) {
+				final InventoryHolder inventoryHolder = (InventoryHolder) block.getState();
+
+				ChipPlugin.cleanInventory(Optional.of(block.getType().name() + " (" + block.getX() + ", " + block.getY() + ", " + block.getZ() + ")"), inventoryHolder.getInventory());
+			}
 		}
 	}
 
